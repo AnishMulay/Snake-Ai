a480206785448e17a19db2033ed6c2ff2853faa2
AnishMulay <anishm7030@gmail.com>
2021-05-24 07:15:29 +0530

    ai agent is fully functional and works well with the game

---
+++ agent.py
@@ -0,0 +1,148 @@
+import torch
+import random
+import numpy as np
+from game import SnakeGameAI, Direction, Point
+from collections import deque
+from model import Linear, Trainer
+from helper import plot
+
+MAX_MEMORY = 100000
+BATCH_SIZE = 1000
+LEARNING_RATE = 0.001
+
+class Agent:
+
+	def __init__(self):
+		self.num_games = 0 # number of games played
+		self.epsilon = 0 # randomness
+		self.gamma = 0.9 # discount rate
+		self.memory = deque(maxlen=MAX_MEMORY) # pops from left if memory limit is exceeded
+		self.model = Linear(11, 256, 3)
+		self.trainer = Trainer(self.model, lr=LEARNING_RATE, gamma=self.gamma)
+
+	def get_state(self, game):
+		head = game.snake[0]
+		point_l = Point(head.x - 20, head.y)
+		point_r = Point(head.x + 20, head.y)
+		point_u = Point(head.x, head.y - 20)
+		point_d = Point(head.x, head.y + 20)
+		
+		dir_l = game.direction == Direction.LEFT
+		dir_r = game.direction == Direction.RIGHT
+		dir_u = game.direction == Direction.UP
+		dir_d = game.direction == Direction.DOWN
+
+		state = [
+			# Danger straight
+			(dir_r and game.is_collision(point_r)) or 
+			(dir_l and game.is_collision(point_l)) or 
+			(dir_u and game.is_collision(point_u)) or 
+			(dir_d and game.is_collision(point_d)),
+
+			# Danger right
+			(dir_u and game.is_collision(point_r)) or 
+			(dir_d and game.is_collision(point_l)) or 
+			(dir_l and game.is_collision(point_u)) or 
+			(dir_r and game.is_collision(point_d)),
+
+			# Danger left
+			(dir_d and game.is_collision(point_r)) or 
+			(dir_u and game.is_collision(point_l)) or 
+			(dir_r and game.is_collision(point_u)) or 
+			(dir_l and game.is_collision(point_d)),
+			
+			# Move direction
+			dir_l,
+			dir_r,
+			dir_u,
+			dir_d,
+			
+			# Food location 
+			game.food.x < game.head.x,  # food left
+			game.food.x > game.head.x,  # food right
+			game.food.y < game.head.y,  # food up
+			game.food.y > game.head.y  # food down
+			]
+
+		return np.array(state, dtype=int)
+
+	def remember(self, state, action, reward, next_state, done):
+		self.memory.append((state, action, reward, next_state, done))
+
+	def train_long_memory(self):
+		if len(self.memory) > BATCH_SIZE:
+			mini_sample = random.sample(self.memory, BATCH_SIZE)
+		else:
+			mini_sample = self.memory
+
+		states, actions, rewards, next_states, dones = zip(*mini_sample)
+		self.trainer.train_step(states, actions, rewards, next_states, dones)
+
+	def train_short_memory(self, state, action, reward, next_state, done):
+		self.trainer.train_step(state, action, reward, next_state, done)
+
+	def get_action(self, state):
+		# initially the agent performs more of random moves i.e exploration
+		self.epsilon = 80 - self.num_games
+		final_move = [0, 0, 0]
+		
+		if random.randint(0, 200) < self.epsilon:
+			move = random.randint(0, 2)
+			final_move[move] = 1
+		else:
+			state0 = torch.tensor(state, dtype=torch.float)
+			prediction = self.model(state0)
+			move = torch.argmax(prediction).item()
+			final_move[move] = 1 
+
+		return final_move
+
+def train():
+	plot_scores = []
+	plot_avg_scores = []
+	total_score = 0
+	max_score = 0
+	agent = Agent()
+	game = SnakeGameAI()
+	while True:
+		# get current state of the game
+		state_old = agent.get_state(game)
+
+		# get the move based on the current state
+		final_move = agent.get_action(state_old)
+
+		# actually perform the move in the game
+		reward, done, score = game.play_step(final_move)
+		state_new = agent.get_state(game)
+
+		# training the short term memory of the agent
+		agent.train_short_memory(state_old, final_move, reward, state_new, done)
+
+		# remember this iteration
+		agent.remember(state_old, final_move, reward, state_new, done)
+
+		if done:
+			# reset the environment for another game
+			game.reset()
+
+			# increase game counter
+			agent.num_games += 1
+
+			# train the long term memory after a game
+			agent.train_long_memory()
+
+			# update the highscore if necessary
+			if score > max_score:
+				max_score = score
+				agent.model.save()
+
+			print('Game', agent.num_games, 'Score', score, 'Highscore', max_score)
+
+			plot_scores.append(score)
+			total_score += score
+			avg_score = total_score/agent.num_games
+			plot_avg_scores.append(avg_score)
+			plot(plot_scores, plot_avg_scores)
+
+if __name__ == '__main__':
+	train()
